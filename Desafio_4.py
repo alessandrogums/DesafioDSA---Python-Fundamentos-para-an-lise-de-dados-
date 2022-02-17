7d246e8faff3525d9d2e62b20eb894e5e16e6071
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-17 16:33:34 -0300

    Create Desafio_4.py

---
+++ Desafio_4.py
@@ -0,0 +1,13 @@
+class SelectionSort(object):
+    def __init__(self,vetor):
+        self.vetor=vetor
+
+    def sortear(self):
+        try:
+            for x in range(len(self.vetor)):
+                for y in range(x+1,len(self.vetor)):
+                    if   self.vetor[y] < self.vetor[x]:
+                        self.vetor[x],self.vetor[y]= self.vetor[y],self.vetor[x]
+            return self.vetor and len(self.vetor)
+        except TypeError:
+           return 'None não pode ser avaliado!'
