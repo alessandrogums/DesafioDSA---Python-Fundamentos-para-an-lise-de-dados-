4820ee87c75c5eda3d0d81b64925990cda5ba90e
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-17 15:19:29 -0300

    Create Desafio_2.py

---
+++ Desafio_2.py
@@ -0,0 +1,23 @@
+
+#Solução análoga ao desafio num 36 do repositório de soluções do codewars 
+
+import math
+
+class Primes:
+    
+    lista_primos=[2]
+    num=3
+    
+    @classmethod 
+    def first(cls,numero):
+        while len(cls.lista_primos) < numero:
+            valida_primo=True
+            raiz_num=int(math.sqrt(cls.num))+1
+            for k in range(2,raiz_num):
+                if cls.num % k == 0 :
+                    valida_primo=False 
+                    break
+            if valida_primo:
+                cls.lista_primos.append(cls.num)
+            cls.num+=2
+        return cls.lista_primos[:numero]
