2ed6fbe3f451a524ab131dd1e3b349dcbcb7990b
Alessandro Gums <90655009+alessandrogums@users.noreply.github.com>
2022-02-17 15:16:59 -0300

    Create Desafio_1.py

---
+++ Desafio_1.py
@@ -0,0 +1,34 @@
+# primeira forma 
+
+def Caracteres_unicos(string):
+    string_fin=''
+    if string is None :
+        return False 
+    else: 
+        for carac in string:
+            if carac  in string_fin:
+                return False
+            string_fin+=carac
+        return True 
+
+      
+# Segunda forma 
+
+def Caracteres_unicos(string):
+    if string is None:
+        return False
+    else:
+        for k,v in enumerate(string):
+            if string.index(v) != k:
+                return False
+        return True 
+
+#terceira forma 
+def Caracteres_unicos(string):
+  if string is None:
+    return False
+  else:
+    uni=set(string)
+    if len(string) - len(uni)  == 0:
+        return True
+    return False 
